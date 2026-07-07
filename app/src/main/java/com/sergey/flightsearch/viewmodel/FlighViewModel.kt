e1ed42888a818e6341a2f337f97c8e2fe2e36afa
Sergey <basergey4@gmail.com>
2026-07-07 12:55:33 +0400

    feat: add viewmodel

---
+++ app/src/main/java/com/sergey/flightsearch/viewmodel/FlighViewModel.kt
@@ -0,0 +1,27 @@
+package com.sergey.flightsearch.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sergey.flightsearch.entity.Airport
+import com.sergey.flightsearch.repository.FlightRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+
+class FlightViewModel(
+    private val repository: FlightRepository
+) : ViewModel() {
+
+    private val _airports = MutableStateFlow<List<Airport>>(emptyList())
+    val airports: StateFlow<List<Airport>> = _airports.asStateFlow()
+
+    fun search(query: String) {
+        repository.searchAirports(query)
+            .onEach {
+                _airports.value = it
+            }
+            .launchIn(viewModelScope)
+    }
+}
